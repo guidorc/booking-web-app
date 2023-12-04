9d0a1f7b74cf92e8cbb50d7c831aaa0499cc2554
guidorc <guido.rc98@gmail.com>
2023-12-04 16:48:55 -0300

    Added user authenthication to frontend

--- app.js
+++ app.js
@@ -11,6 +11,18 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
+
 app.use(checkAuth);
 
 app.use(
@@ -27,7 +39,7 @@ mongoose
     `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.nnh0nvs.mongodb.net/${process.env.MONGO_DB}`
   )
   .then(() => {
-    app.listen(3000);
+    app.listen(8000);
   })
   .catch((err) => {
     console.log(err);
--- frontend/src/pages/Auth.js
+++ frontend/src/pages/Auth.js
@@ -1,8 +1,93 @@
 import React, { Component } from "react";
 
+import "./Auth.css";
+
 class AuthPage extends Component {
+  state = {
+    isSignin: true,
+  };
+
+  constructor(props) {
+    super(props);
+    this.emailEl = React.createRef();
+    this.passwordEl = React.createRef();
+  }
+
+  switchModeHandler = () => {
+    this.setState((prevState) => {
+      return { isSignin: !prevState.isSignin };
+    });
+  };
+
+  submitHandler = async (event) => {
+    event.preventDefault();
+    const email = this.emailEl.current.value;
+    const password = this.passwordEl.current.value;
+
+    if (email.trim().length === 0 || password.trim().length === 0) {
+      return;
+    }
+
+    let requestBody = {
+      query: `
+        query {
+          signin(userInput: {email: "${email}", password: "${password}"}) {
+            userId
+            token
+            tokenExpiration
+          }
+        }
+      `,
+    };
+
+    if (!this.state.isSignin) {
+      requestBody = {
+        query: `
+          mutation {
+            signup (userInput: {email: "${email}", password: "${password}"}) {
+              _id
+              email
+            }
+          }
+        `,
+      };
+    }
+
+    const response = await fetch("http://localhost:8000/graphql", {
+      method: "POST",
+      body: JSON.stringify(requestBody),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (response.status != 200 && response.status != 201) {
+      throw new Error("Sign up failed");
+    }
+
+    const data = await response.json();
+    console.log({ data });
+  };
+
   render() {
-    return <h1>Auth Page</h1>;
+    return (
+      <form className="auth-form" onSubmit={this.submitHandler}>
+        <div className="form-control">
+          <label htmlFor="email">E-Mail</label>
+          <input type="email" id="email" ref={this.emailEl}></input>
+        </div>
+        <div className="form-control">
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" ref={this.passwordEl}></input>
+        </div>
+        <div className="form-actions">
+          <button type="submit">Submit</button>
+          <button type="button" onClick={this.switchModeHandler}>
+            Switch to {this.state.isSignin ? "Signup" : "Signin"}
+          </button>
+        </div>
+      </form>
+    );
   }
 }
 
